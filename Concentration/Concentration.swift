39096b7b0e73bced5de629d112eaa3927ac3a6a0
GrigorVladislav <skaar656236@gmail.com>
2019-04-17 07:45:19 -0700

    AutoLayout

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,53 @@
+
+
+import Foundation
+
+class Concentration {
+	
+	private(set) var cards = [Card]()
+	private var indexOfOneAndOnlyFaceUpCard: Int? {
+		get {
+			return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+		}
+		set {
+			for index in cards.indices {
+				cards[index].isFaceUp = (index == newValue)
+			}
+		}
+				
+	}
+	
+	func chooseCard(at index: Int) {
+		assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)) : Choosen index out of range")
+		if !cards[index].isMatched {
+			if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+				if cards[matchIndex] == cards[index] {
+					cards[matchIndex].isMatched = true
+					cards[index].isMatched = true
+				}
+				cards[index].isFaceUp = true
+			} else {
+				indexOfOneAndOnlyFaceUpCard = index
+			}
+		}
+	}
+	
+	init(numberOfPairsOfCards: Int) {
+		assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)) : You must have at least one pair of cards")
+		for _ in 1...numberOfPairsOfCards {
+			let card = Card()
+			cards += [card, card]
+		}
+
+	}
+}
+
+extension Collection {
+	var oneAndOnly: Element? {
+		return count == 1 ? first : nil
+	}
+}
+
+
+
+
---
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,54 @@
+
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController,
+                                    UISplitViewControllerDelegate {
+
+	let themes = [
+		"Sports": "⚽️🏀🏈⚾️🎾🏐🏉🎱🏓⛷🎳⛳️",
+		"Animals": "🐶🦆🐹🐸🐘🦍🐓🐩🐦🦋🐙🐏",
+		"Faces": "😀😌😎🤓😠😤😭😰😱😳😜😇"
+	]
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+		splitViewController?.delegate = self
+	}
+	
+	@IBAction func chooseTheme(_ sender: Any) {
+
+			performSegue(withIdentifier: "Choose Theme", sender: sender as! UIButton)
+
+	}
+	
+	private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+		return splitViewController?.viewControllers.last as? ConcentrationViewController
+	}
+	
+	private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+	
+	func splitViewController(_ splitViewController: UISplitViewController,
+							 collapseSecondary secondaryViewController: UIViewController,
+							 onto primaryViewController: UIViewController
+		) -> Bool
+	{
+		if let cvc = secondaryViewController as? ConcentrationViewController {
+			return cvc.theme == nil
+		}
+		return false
+	}
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let identifier = segue.identifier, identifier == "Choose Theme", let cvc = segue.destination as? ConcentrationViewController {
+			if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+				cvc.theme = theme
+				lastSeguedToConcentrationViewController = cvc
+			}
+		}
+		
+    }
+
+
+}
